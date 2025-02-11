708edf9efd7fa0acdc773939d6fcfa8b62ae82b8
Helena <westermannhelena@gmail.com>
2025-02-11 17:18:35 +0100

    Add launch file and script to start and stop mapping

---
+++ packages/ari_milestone1/mapper/scripts/mapping.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+import sys
+import rospy
+from std_srvs.srv import Empty
+from actionlib import SimpleActionClient
+from hri_msgs.msg import LiveSpeech
+from pal_navigation_msgs.srv import Acknowledgment, AcknowledgmentRequest
+from pal_interaction_msgs.msg import TtsAction, TtsGoal
+
+class Mapper(object):
+    
+    def __init__(self):
+        rospy.loginfo("Talker::init node")
+
+        # Connect to the text-to-speech action server
+        self.tts = SimpleActionClient('/tts', TtsAction)
+        self.tts.wait_for_server()
+        rospy.loginfo("RobotBase::Connected to TTS action client")
+
+        self.speech_sub = rospy.Subscriber('/humans/voices/anonymous_speaker/speech', LiveSpeech, self.speech_cb)
+        rospy.loginfo("RobotBase::Subscribed to " + self.speech_sub.resolved_name)
+
+        rospy.wait_for_service('pal_navigation_sm')
+        rospy.wait_for_service('/pal_map_manager/start_map')
+        rospy.wait_for_service('/pal_map_manager/stop_map')
+
+        self.navigation_mode = rospy.ServiceProxy('pal_navigation_sm', Acknowledgment)
+        self.start_map = rospy.ServiceProxy('pal_map_manager/start_map', Empty)
+        self.stop_map = rospy.ServiceProxy('pal_map_manager/stop_map', Empty)
+        self.change_map = rospy.ServiceProxy('pal_map_manager/change_map', Acknowledgment)
+        
+
+    def start_mapping(self):
+        self.talk("Starting mapping")
+
+        request = AcknowledgmentRequest()
+        request.input = 'MAP'
+        response = self.navigation_mode(request)
+
+
+    def talk(self, text):
+        rospy.loginfo("I'll say: " + text)
+        
+        # Create a goal to say our sentence
+        goal = TtsGoal()
+        goal.rawtext.text = text
+        goal.rawtext.lang_id = "en_GB"
+
+        # Send the goal and wait
+        self.tts.send_goal_and_wait(goal)
+        
+
+    def end_mapping(self):
+        self.talk("Stopping mapping")
+
+        request = AcknowledgmentRequest()
+        request.input = 'LOC'
+        response = self.navigation_mode(request)
+
+        request = AcknowledgmentRequest()
+        request.input = '2025-02-11_135553'
+        response = self.change_map(request)
+
+
+        
+    def speech_cb(self, data):
+
+        # When speech is published, say the text of the speech
+        if data.final == "stop mapping":
+            self.end_mapping()
+
+        if data.final == "start mapping":
+            self.start_mapping()
+            
+
+
+if __name__ == '__main__':
+
+    try:
+        # Initialize the node
+        rospy.init_node('mapper_node', anonymous=True)
+        mapper = Mapper()
+
+        rospy.spin()
+        
+    except rospy.ROSInterruptException:
+        pass
+
