7803973e920888a94f3ad1cbbb18ca1284acf59b
Helena <westermannhelena@gmail.com>
2025-03-05 17:22:55 +0100

    Add files for finding speaker

---
+++ packages/dd2414_speech/src/detect_people.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import rospy
+from tf import transformations as t
+import numpy as np
+import pyhri
+from hri_msgs.msg import IdsList
+from std_msgs.msg import String
+from geometry_msgs.msg import PointStamped
+
+class BodyOrientationListener:
+
+    def __init__(self,
+
+                base_frame = "head_front_camera_link",
+
+                 threshold=30):
+
+        self.hri_listener = pyhri.HRIListener()
+        self.base_frame = base_frame
+        self.threshold = threshold
+
+        self.id_pub = rospy.Publisher('person_looking_at_robot', String, queue_size=10)
+
+        self.rate = rospy.Rate(1)
+        self.is_body_facing_robot = False
+
+
+    def run(self):
+
+        """ The run function implement the main
+
+            functionality of the BodyOrientationListener
+
+            object, that is understanding which bodies
+
+            are oriented toward the robot. Here, the
+
+            base_frame specified during the object
+
+            initialisation represent the robot. """
+
+        while not rospy.is_shutdown():
+
+            bodies = self.hri_listener.bodies.items()
+            bodies_facing_robot = []
+
+            for body in bodies:
+
+                transform = body[1].transform(self.base_frame)
+                trans = transform.transform.translation
+                rot = transform.transform.rotation
+
+                translation_matrix = t.translation_matrix((trans.x, trans.y, trans.z))
+                quaternion_matrix = t.quaternion_matrix((rot.x, rot.y, rot.z, rot.w))
+                transform = t.concatenate_matrices(translation_matrix, quaternion_matrix)
+                inv_transform = t.inverse_matrix(transform)
+                b2r_translation_x = inv_transform[0, 3]
+                b2r_translation_y = inv_transform[1, 3]
+                b2r_xy_norm = np.linalg.norm([b2r_translation_x, b2r_translation_y], ord = 2)
+                
+                # Does the base frame lie inside the body frame-based cone of attention?
+
+                if np.arccos(b2r_translation_x/b2r_xy_norm) < (self.threshold/180*np.pi) and b2r_translation_x > 0:
+
+                    bodies_facing_robot.append(body[0])
+                    print("namespace: ", body[1].ns)
+                    print("body: ", body[1])
+                    self.id_pub.publish((body[0]))
+            
+            if(len(bodies_facing_robot) == 0):
+                self.id_pub.publish("")
+
+
+            self.rate.sleep()
+
+
+if __name__=="__main__":
+
+
+    rospy.init_node("body_orientation_listener")
+    bol = BodyOrientationListener(threshold=20)
+    bol.run()
---
+++ packages/dd2414_speech/src/find_speaker.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+
+import rospy
+import actionlib
+import tf2_ros as tf
+import numpy as np
+import pyhri
+from actionlib import SimpleActionClient
+from std_msgs.msg import Int32, Bool, String
+from std_srvs.srv import Empty, EmptyResponse
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from tf.transformations import quaternion_from_euler
+from dd2414_speech.msg import FindSpeakerAction, FindSpeakerResult
+
+class FindSpeakerActionServer(object):
+    
+    def __init__(self):
+
+        self.server = actionlib.SimpleActionServer(
+            "find_latest_speaker", FindSpeakerAction, self.execute_cb, False
+        )
+        self.server.start()
+        self.result = FindSpeakerResult()
+
+        self.directions = []
+        self.speaking_in_progress = False
+        self.turning_to_speech = False
+        self.finding_speaker_active = False
+        self.turning_goal = MoveBaseGoal()
+
+        self.hri_listener = pyhri.HRIListener()
+
+        self.speech_sub = rospy.Subscriber('/humans/voices/anonymous_speaker/is_speaking', Bool, self.speech_cb, queue_size=10)
+        rospy.loginfo(f"FindSpeaker::Subscribed to {self.speech_sub.resolved_name}")
+
+        self.person_found_sub = rospy.Subscriber('person_looking_at_robot', String, self.person_found_cb, queue_size=10)
+        rospy.loginfo(f"FindSpeaker::Subscribed to {self.person_found_sub.resolved_name}")
+
+        self.direction_sub = rospy.Subscriber('/audio/sound_direction', Int32, self.direction_cb)
+        rospy.loginfo(f"FindSpeaker::Subscribed to {self.direction_sub.resolved_name}")
+
+        self.tfBuffer = tf.Buffer()
+        self.listener = tf.TransformListener(self.tfBuffer)
+        rospy.loginfo("FindSpeaker::TF listener initialized")
+
+        self.move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.move_base_client.wait_for_server()
+        
+
+
+    def execute_cb(self, data):
+        rospy.loginfo("FindSpeaker::Request to look at speaker received")
+        self.person_found = False
+        self.finding_speaker_active = True
+
+        if(len(self.directions) > 0):
+            median_direction = np.median(self.directions)
+            
+            self.rotate_to(median_direction)
+            self.directions = []
+        else:
+            self.result.success = False
+            self.server.set_succeeded(self.result)
+            self.finding_speaker_active = False
+
+        while self.finding_speaker_active:
+            if self.server.is_preempt_requested():
+                rospy.loginfo("FindSpeaker::Goal preempted")
+                self.move_base_client.cancel_goal(self.turning_goal)
+                self.finding_speaker_active = False
+                self.server.set_preempted()  # Mark the goal as preempted
+                return 
+        
+    
+    def rotate_to(self, direction):
+
+        rospy.loginfo(f"FindSpeaker::Sending request to rotate in direction {direction}")
+
+        self.turning_to_speech = True
+        rotation_rad = np.deg2rad(-2*direction)
+
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "base_link"
+        goal.target_pose.header.stamp = rospy.Time.now()
+
+        quat = quaternion_from_euler(0, 0, rotation_rad)
+        goal.target_pose.pose.orientation.x = quat[0]
+        goal.target_pose.pose.orientation.y = quat[1]
+        goal.target_pose.pose.orientation.z = quat[2]
+        goal.target_pose.pose.orientation.w = quat[3]
+
+        self.move_base_client.send_goal(goal)
+        self.turning_goal = goal
+        self.move_base_client.wait_for_result()
+
+        self.turning_to_speech = False
+
+        # Wait to see if body is found during or after rotation
+        rospy.wait(1)
+        if(self.finding_speaker_active):
+            rospy.loginfo("FindSpeaker::Finished turning towards speech, could not find body")
+            self.result.succeeded = False
+            self.server.set_succeeded(self.result)
+            self.finding_speaker_active = False
+
+
+        rospy.loginfo("FindSpeaker::Finished turning towards speech")
+
+    # When body looking at ari is detected, turn to body
+    def person_found_cb(self, data):
+        if(data.data != "" and self.finding_speaker_active):
+            rospy.loginfo("FindSpeaker::Foudn body looking at ari")
+            self.finding_speaker_active = False
+            self.result.success = True
+            
+            if(self.turning_to_speech):
+                self.move_base_client.cancel_goal(self.turning_goal)
+                self.turning_to_speech = False
+
+            goal = MoveBaseGoal()
+            body = self.hri_listener.bodies.get(data.data)
+
+            try:
+                transform = body.transform("base_link").transform
+
+                target_x = transform.translation.x
+                target_y = transform.translation.y
+
+                yaw = np.arctan2(target_y, target_x)
+                quat = quaternion_from_euler(0, 0, yaw)
+
+                goal = MoveBaseGoal()
+
+                goal.target_pose.header.frame_id = 'base_link'
+                
+                goal.target_pose.pose.orientation.x = quat[0]
+                goal.target_pose.pose.orientation.y = quat[1]
+                goal.target_pose.pose.orientation.z = quat[2]
+                goal.target_pose.pose.orientation.w = quat[3]
+
+                goal.target_pose.header.stamp = rospy.Time.now()
+
+                self.turning_goal = goal
+                self.move_base_client.send_goal(goal)
+                wait = self.move_base_client.wait_for_result()
+                rospy.loginfo("FindSpeaker::Finished turning to body")
+
+                
+            except:
+                rospy.loginfo("FindSpeaker::Could not transform body frame")
+
+            self.server.set_succeeded(self.result)
+
+    # Check for speech, always active
+    def speech_cb(self, data):
+        if(data.data):
+            rospy.loginfo("FindSpeaker::Speech started, recording direction")
+            if(not self.speaking_in_progress):
+                self.directions = []
+            self.speaking_in_progress = True
+            return
+        else:
+            rospy.loginfo("FindSpeaker::Speech stopped, stopped recording direction")
+            self.speaking_in_progress = False
+    
+    # Save directions, active during speech
+    def direction_cb(self, data):        
+        if(self.speaking_in_progress):
+            direction = data.data
+            print(direction)
+            self.directions.append(data.data)
+
+if __name__ == '__main__':
+
+    try:
+        # Initialize the node
+        rospy.init_node('talking', anonymous=True)
+        findSpeaker = FindSpeakerActionServer()
+
+        rospy.spin()
+        
+    except rospy.ROSInterruptException:
+        pass
+
