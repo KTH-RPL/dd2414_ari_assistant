8253ea7cb1e9ea6a2401225824af3edfc0749516
David Flores <davidfd2008@gmail.com>
2025-05-06 15:43:46 +0200

    Remeber User Action added to behaviour tree

--- packages/dd2414_brain_v2/src/behaviour_tree.py
+++ packages/dd2414_brain_v2/src/behaviour_tree.py
@@ -35,7 +35,7 @@ class Brain:
         self.namespace_dict = {
             "test"                 :"/test",
             #"stop"                 :self.stop,
-            #"remember user"        :self.name_assign,
+            "remember user"        :"/face_recognition_node",
             "go to"                :"/move_to_poi",
             "find speaker"         :"/ari_turn_to_speaker",
             "follow user"          :"/follow_user",
@@ -74,7 +74,7 @@ class Brain:
         self.action_dict = {
             "test"                 :TestBehaviour(name="test behaviour"),
             #"stop"                 :self.stop,
-            #"remember user"        :self.name_assign,
+            "remember user"        :self.behaviours['remember user'],
             "go to"                :self.behaviours['go to'],
             "find speaker"         :self.behaviours['find speaker'],
             "follow user"          :follow_user_behaviour,
