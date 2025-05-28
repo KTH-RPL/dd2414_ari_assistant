caa83899e9c5c661c8bb51ad9750089e30c957a3
David Flores <davidfd2008@gmail.com>
2025-05-28 12:03:06 +0200

    Parameter Server changes dynamically on door zones

--- packages/dd2414_parameter_server/src/parameter_server.py
+++ packages/dd2414_parameter_server/src/parameter_server.py
@@ -2,28 +2,79 @@
 
 import rospy
 from dynamic_reconfigure.client import Client
+from pal_zoi_detector.msg import CurrentZoI
+from std_srvs.srv import Empty
+class ParamServer:
 
-def update_inflation_radius():
-    rospy.init_node('inflation_radius_updater')
-    header_string = "[PARAM. SERVER  ]:"
+    def __init__(self):
+        rospy.init_node('inflation_radius_updater')
+        self.curr_zoi = rospy.Subscriber("/current_zone_of_interest",CurrentZoI,self.cb_current_zoi)
+        self.rate = rospy.Rate(1)
+        self.header_string = "[PARAM. SERVER  ]:"
 
-    rospy.loginfo(header_string + "Initialized")
-    # Create clients for both global and local inflation layers
-    global_client = Client("/move_base/global_costmap/inflation_layer", timeout=5)
-    global_robot_radius = rospy.get_param("/move_base/global_costmap/robot_radius",0.5)
-    local_client = Client("/move_base/local_costmap/inflation_layer", timeout=5)
-    local_robot_radius = rospy.get_param("/move_base/local_costmap/robot_radius",0.5)
+        self.global_client = Client("/move_base/global_costmap/inflation_layer", timeout=5)
+        self.local_client = Client("/move_base/local_costmap/inflation_layer", timeout=5)
+        self.local_rgbd = Client("/move_base/local_costmap/stvl_rgbd_layer", timeout=5)
+        self.current_status = False
+        self.currently_at_door = False
 
-    # Set new values for the inflation radius
-    new_radius = 0.7  # Example: update to 0.7 meters
+        
+    def update_inflation_radius(self):
 
-    global_client.update_configuration({"inflation_radius": global_robot_radius})
-    rospy.loginfo(header_string + "Updated global inflation radius to %.2f", global_robot_radius)
+        rospy.loginfo(self.header_string + "Initialized")
+        # Create clients for both global and local inflation layers
+        self.global_robot_radius = rospy.get_param("/move_base/global_costmap/robot_radius",0.5)
+        self.local_robot_radius = rospy.get_param("/move_base/local_costmap/robot_radius",0.5)
 
-    local_client.update_configuration({"inflation_radius": local_robot_radius})
-    rospy.loginfo(header_string + "Updated local inflation radius to %.2f", local_robot_radius)
-    rospy.loginfo(header_string + "Done")
+        # Set new values for the inflation radius
+        new_radius = 0.7  # Example: update to 0.7 meters
 
+        self.global_client.update_configuration({"inflation_radius": self.global_robot_radius})
+        rospy.loginfo(self.header_string + "Updated global inflation radius to %.2f", self.global_robot_radius)
+
+        self.local_client.update_configuration({"inflation_radius": self.local_robot_radius})
+        rospy.loginfo(self.header_string + "Updated local inflation radius to %.2f", self.local_robot_radius)
+
+        self.mark_threshold = 10
+        self.local_rgbd.update_configuration({"mark_threshold":self.mark_threshold})
+        rospy.loginfo(self.header_string + "Updated local rgbd mark threshold to %.2f", self.mark_threshold)
+        rospy.loginfo(self.header_string + "Done")
+
+    def dynamic_update(self):
+        #if there is a change in status perform the param server update
+        if self.current_status != self.currently_at_door:
+            rospy.loginfo(f"Updated Server: Currently at door {self.currently_at_door}")
+            if self.currently_at_door:
+                self.global_client.update_configuration({"inflation_radius": 0.1})
+                self.local_client.update_configuration({"inflation_radius": 0.1})
+                try:
+                    service_proxy = rospy.ServiceProxy('/move_base/clear_costmaps',Empty)
+                    response = service_proxy()  # Call with argument(s)
+                    print("Service response:", response)
+                except rospy.ServiceException as e:
+                    print("Service call failed:", e)
+            else:
+                self.global_client.update_configuration({"inflation_radius": self.global_robot_radius})
+                self.local_client.update_configuration({"inflation_radius": self.local_robot_radius})
+            
+            self.current_status = self.currently_at_door
+                
+
+
+        self.rate.sleep()
+
+    def cb_current_zoi(self,msg):
+        for zoi in msg.zois:
+            if "door_" in zoi:
+                self.currently_at_door = True
+#                rospy.loginfo("In Door ZOI")
+                return
+            
+#        rospy.loginfo("Not In Door ZOI")
+        self.currently_at_door = False
 
 if __name__ == "__main__":
-    update_inflation_radius()
+    node = ParamServer()
+    node.update_inflation_radius()
+    while not rospy.is_shutdown():
+        node.dynamic_update()
