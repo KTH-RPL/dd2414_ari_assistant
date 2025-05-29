1fec8624fb94218e82a12191440b33101b531cb6
Helena <westermannhelena@gmail.com>
2025-05-29 17:33:19 +0200

    Cleaned up behaviour tree

--- packages/dd2414_brain_v2/src/behaviour_tree.py
+++ packages/dd2414_brain_v2/src/behaviour_tree.py
@@ -4,12 +4,12 @@
 import operator
 import os
 import rospy
+import json
 from std_msgs.msg import String
 from stop_behaviour import StopBehaviour
 from exploration_node import ExploreBehaviour
 from rospy.exceptions import ROSException
 import dd2414_brain_v2.msg as brain
-from dd2414_brain_v2.msg import BrainAction 
 
 import actionlib
 
@@ -17,22 +17,17 @@ import py_trees
 import py_trees_ros
 
 
-#Dont MOVE ANYTING FROM HERE
 class Brain:
     def __init__(self):
 
         rospy.loginfo("Brain Start Initializing")
 
         self.intent_sub=rospy.Subscriber(rospy.get_param("~IN_intent_topic","~intent"),String, self.intent_cb, queue_size=1)
-
-        self.intent_dict = {}
-        self.intent = ""
-
         self.blackboard = py_trees.blackboard.Blackboard()
-        self.current_requested_intent = None
 
+        # Namespaces for action servers (if connected to an intent, name should be the same)
         self.namespace_dict = {
-            "stop"                 :"/stop",
+            "stop"                 :"/ollama_response",
             "translate"            :"/translate_conversation",
             "move to translate"    :"/body_orientation_listener",
             "remember user"        :"/face_recognition_node",
@@ -40,160 +35,98 @@ class Brain:
             "go to"                :"/move_to_poi",
             "find speaker"         :"/find_speaker",
             "follow user"          :"/follow_user",
-            #"move to person"       :"/body_orientation_listener",
             "provide information"  :"/ollama_response",
-            #"speech"               :self.text_to_speech,
             "greet"                :"/face_recognition_node",
             "goodbye"              :"/face_recognition_node",
-            "explore"              :"/explore",
-            "reject"               :"/ollama_response"
+            "explore"              :"/ollama_response",
+            "reject"               :"/ollama_response",
+            "look at person"       :"/face_gaze_tracker",
+            "find object"          :"/move_to_poi"
         }
 
-
-        self.behaviours = {}
+        # Set up publishers for goals
         self.publishers_dict = {}
 
         for action in self.namespace_dict:
             try:
-                
-                behaviour = py_trees_ros.actions.ActionClient(
-                    name=action,
-                    action_namespace=self.namespace_dict[action],
-                    action_spec=brain.BrainAction,
-                    action_goal=brain.BrainGoal())
-                self.behaviours[action] = behaviour
-
                 publisher = rospy.Publisher(f"/brain{self.namespace_dict[action]}", brain.BrainGoal, queue_size=1)
                 self.publishers_dict[action] = publisher
             except ROSException as e:
                 rospy.logwarn(f"Could not connect to {self.namespace_dict[action]}")
 
+        # Set up more complex behaviours
         [look_at_face_behaviour, stop_look_at_face_behaviour] = self.setup_look_at_face()
-        
-        
         go_to_behaviour = py_trees.Sequence(
             "Stop looking at person, go to",
-            [stop_look_at_face_behaviour, self.behaviours['go to'], look_at_face_behaviour]
+            [stop_look_at_face_behaviour, self.create_behaviour_from_action_server("go to"), look_at_face_behaviour]
         )
 
         follow_user_behaviour = py_trees.Sequence(
             "Find speaker, then follow user", 
             #[stop_look_at_face_behaviour,
-            [self.behaviours["find speaker"],  
-             self.behaviours["follow user"]]
+            [self.create_behaviour_from_action_server("find speaker"),  
+             self.create_behaviour_from_action_server("follow user")]
              #look_at_face_behaviour]
              )
         
         stop_behaviour = py_trees.Sequence(
             "Stop, look at person",
             [StopBehaviour(name="stop behaviour", action_dict=self.namespace_dict), 
+             self.create_behaviour_from_action_server("stop"),
              look_at_face_behaviour]
         )
 
+        translate_behaviour = py_trees.Sequence(
+            "Find speaker, then move next to them and translate", 
+            [stop_look_at_face_behaviour,
+             #self.create_behaviour_from_action_server("find speaker"),
+             #self.create_behaviour_from_action_server("move to translate"),
+             self.create_behaviour_from_action_server("translate")])
 
+        explore_behaviour = py_trees.Sequence(
+            "Explore", 
+            [self.create_behaviour_from_action_server("explore"),
+             ExploreBehaviour(name = "explore"),
+             ])
+
+        # Define behaviours for each intent
         self.action_dict = {
-            #"stop"                 :stop_behaviour,
-            "translate"            :self.setup_translate_behaviour(),
+            "stop"                 :stop_behaviour,
+            "translate"            :translate_behaviour,
             "remember user"        :self.setup_greet_behaviour(),
-            #"face recognition"     :self.behaviours["face recognition"],
             "go to"                :go_to_behaviour,
-            #"find speaker"         :self.behaviours['find speaker'],
+            "find object"          :go_to_behaviour,
             "follow user"          :follow_user_behaviour,
-            "provide information"  :self.behaviours['provide information'],
-            #"speech"               :self.text_to_speech,
+            "provide information"  :self.create_behaviour_from_action_server("provide information"),
             "greet"                :self.setup_greet_behaviour(),
             "goodbye"              :self.setup_greet_behaviour(),
-            "explore"              :ExploreBehaviour(name = "explore"),
-            "reject"               :self.behaviours['reject']
+            "explore"              :explore_behaviour,
+            "reject"               :self.create_behaviour_from_action_server("reject")
             }      
 
+        # Set up the failcounters and action requests in the blackboard
         for action in self.action_dict:
             self.blackboard.set(action, False)
             self.blackboard.set(f"failcounter {action}", 0)
 
-        # Set up behaviours and construct tree
+        # Construct tree
         root = py_trees.composites.Parallel(policy=py_trees.common.ParallelPolicy.SUCCESS_ON_ALL)
 
-        # For every action, set up condition (action requested) and behaviour (action)
         for action in self.action_dict:
 
             if(not self.service_is_available(self.namespace_dict[action]) and not action=="stop" and not action=="explore"):
                 rospy.logwarn(f"Service {self.namespace_dict[action]} not available, skipping {action} behaviour")
-                #root.add_child(py_trees.behaviours.Success(name=f"Mock {action}"))
                 continue
-            
-            behaviour = self.action_dict[action]
 
-            # Create condition to check if action is requested
-            condition = py_trees.blackboard.CheckBlackboardVariable(
-                name=f"{action} action not requested?",
-                variable_name=action,
-                expected_value=False
-            )
-
-            reset_fail_counter = py_trees.blackboard.SetBlackboardVariable(
-                name="Set fail counter back to 0",
-                variable_name=f"failcounter {action}",
-                variable_value=0
-            )
-
-            set_action_requested_to_false = py_trees.blackboard.SetBlackboardVariable(
-                name="Set Action requested back to false",
-                variable_name=action,
-                variable_value=False
-            )
-
-            check_fail_counter = py_trees.blackboard.CheckBlackboardVariable(
-                    name=f"{action} failcounter more than 10?",
-                    variable_name=f"failcounter {action}",
-                    expected_value=10,
-                    comparison_operator=operator.ge,
-                    debug_feedback_message=True
-                )
-
-            counter_check = py_trees.composites.Sequence(
-                f"Check fail counter",
-                [
-                check_fail_counter,
-                set_action_requested_to_false,
-                reset_fail_counter
-                ]
-            )
-            # Create behaviour to be executed if action is requested
-            
-
-            incrementor = py_trees.behaviours.Failure(name="IncrementCounter")
-            incrementor.update = (lambda a=action: self.increment_fail_counter(a) or py_trees.common.Status.FAILURE)
-
-
-            # Add condition and behaviour to tree
-            root.add_children([
-                py_trees.composites.Selector(
-                    f"Execute {action} action if requested", 
-                    [
-                        condition,
-                        counter_check,
-                        py_trees.composites.Sequence(
-                            f"Execute {action} action and set action requested to false", 
-                            [
-                            py_trees.composites.Selector(
-                                f"Execute {action} action or increase counter",
-                                [behaviour,
-                                 incrementor]
-
-                            ), 
-                            set_action_requested_to_false,
-                            reset_fail_counter
-                            ]),
-                    ]),
-                ])
+            root.add_child(self.intent_to_behaviour_tree(action))            
 
         self.behaviour_tree = py_trees.trees.BehaviourTree(root=root)
         print(py_trees.display.print_ascii_tree(root=self.behaviour_tree.root))        
-
         self.behaviour_tree.setup(timeout=10)
 
+        # Start looking at faces
         look_at_face_behaviour.tick_once()
+
         rospy.loginfo("Brain Finished Initializing")
 
     def print_tree(self, tree: py_trees.trees.BehaviourTree) -> None:
@@ -206,61 +139,40 @@ class Brain:
 
     def intent_cb(self,string_msg):
 
-        self.intent_dict = json.loads(string_msg.data)
-        self.intent = self.intent_dict["intent"]
-        intent = self.intent
+        intent_dict = json.loads(string_msg.data)
+        intent = intent_dict["intent"]
 
-        self.blackboard.set(self.intent, True)
+        self.blackboard.set(intent, True)
 
-
-        if(self.publishers_dict[intent] and self.intent_dict.get('input')):
+        if(self.publishers_dict[intent] and intent_dict.get('input')):
             goal = brain.BrainGoal()
-            goal.goal=self.intent_dict['input']
-            goal.in_dic = json.dumps(self.intent_dict)
-
+            goal.goal=intent_dict['input']
+            goal.in_dic = json.dumps(intent_dict)
 
             self.publishers_dict[intent].publish(goal)
-    
-    def setup_look_at_face(self):
-        if(self.service_is_available("/face_gaze_tracker")):
-            look_at_face_goal = brain.BrainGoal()
-            look_at_face_goal.goal = "start"
-            look_at_face_behaviour = py_trees_ros.actions.ActionClient(
-                    name="Look at face",
-                    action_namespace="face_gaze_tracker",
-                    action_spec=brain.BrainAction,
-                    action_goal=look_at_face_goal)
-            
-            stop_look_at_face_goal = brain.BrainGoal()
-            stop_look_at_face_goal.goal = "stop"
-            stop_look_at_face_behaviour = py_trees_ros.actions.ActionClient(
-                    name="Stop look at face",
-                    action_namespace="face_gaze_tracker",
-                    action_spec=brain.BrainAction,
-                    action_goal=stop_look_at_face_goal)
-            return [look_at_face_behaviour, stop_look_at_face_behaviour]
-        else:
-            look_at_face_behaviour = py_trees.behaviours.Success(name="Mock look at face")
-            stop_look_at_face_behaviour = py_trees.behaviours.Success(name="Mock stop look at face")
-            return [look_at_face_behaviour, stop_look_at_face_behaviour]
         
+    def create_behaviour_from_action_server(self, action, name = None, goal = None):
+        brainGoal = brain.BrainGoal()
+        if(goal is not None):
+            brainGoal.goal = goal
+
+        behaviourName = action 
+        if(name is not None):
+            behaviourName = name
+
+        behaviour = py_trees_ros.actions.ActionClient(
+            name=behaviourName,
+            action_namespace=self.namespace_dict[action],
+            action_spec=brain.BrainAction,
+            action_goal=brainGoal)
+        
+        return behaviour
+
     def setup_greet_behaviour(self):
         [look_at_face_behaviour, stop_look_at_face_behaviour] = self.setup_look_at_face()
 
-        
-        find_speaker = "find speaker"
-        find_speaker_behaviour = py_trees_ros.actions.ActionClient(
-                    name=find_speaker,
-                    action_namespace=self.namespace_dict[find_speaker],
-                    action_spec=brain.BrainAction,
-                    action_goal=brain.BrainGoal())
-        
-        face_recognition = "face recognition"
-        face_recognition_behaviour = py_trees_ros.actions.ActionClient(
-                    name=face_recognition,
-                    action_namespace=self.namespace_dict[face_recognition],
-                    action_spec=brain.BrainAction,
-                    action_goal=brain.BrainGoal())
+        find_speaker_behaviour = self.create_behaviour_from_action_server("find speaker")
+        face_recognition_behaviour = self.create_behaviour_from_action_server("face recognition")
 
         return py_trees.Sequence(
             "Find speaker, then say hello (with name)", 
@@ -269,44 +181,83 @@ class Brain:
              face_recognition_behaviour,
              look_at_face_behaviour])
     
-    # Move to translate not working
-    def setup_translate_behaviour(self):
-        [look_at_face_behaviour, stop_look_at_face_behaviour] = self.setup_look_at_face()
-
-        find_speaker = "find speaker"
-        find_speaker_behaviour = py_trees_ros.actions.ActionClient(
-                    name=find_speaker,
-                    action_namespace=self.namespace_dict[find_speaker],
-                    action_spec=brain.BrainAction,
-                    action_goal=brain.BrainGoal())
+    def setup_look_at_face(self):
+        if(self.service_is_available(self.namespace_dict["look at person"])):
+            look_at_face = self.create_behaviour_from_action_server("look at person", goal = "start")
+            
+            stop_look_at_face = self.create_behaviour_from_action_server("look at person", name="stop look at person", goal = "stop")
+            
+            return [look_at_face, stop_look_at_face]
         
-        move_to_translate = "move to translate"
-        move_to_translate_behaviour = py_trees_ros.actions.ActionClient(
-                    name=move_to_translate,
-                    action_namespace=self.namespace_dict[move_to_translate],
-                    action_spec=brain.BrainAction,
-                    action_goal=brain.BrainGoal())
+        # If look at face is not available, create mock behaviours so tree doesn't crash
+        else:
+            look_at_face = py_trees.behaviours.Success(name="Mock look at face")
+            stop_look_at_face = py_trees.behaviours.Success(name="Mock stop look at face")
+            return [look_at_face, stop_look_at_face]
+    
+    def intent_to_behaviour_tree(self, action):
         
-        translate = "translate"
-        translate_behaviour = py_trees_ros.actions.ActionClient(
-                    name=translate,
-                    action_namespace=self.namespace_dict[translate],
-                    action_spec=brain.BrainAction,
-                    action_goal=brain.BrainGoal())
-        
-        return py_trees.Sequence(
-            "Find speaker, then move next to them and translate", 
-            [stop_look_at_face_behaviour,
-             #find_speaker_behaviour, 
-             #move_to_translate_behaviour,
-             translate_behaviour])
-                
+        behaviour = self.action_dict[action]
 
+        # Create condition to check if action is requested
+        condition = py_trees.blackboard.CheckBlackboardVariable(
+            name=f"{action} action not requested?",
+            variable_name=action,
+            expected_value=False
+        )
 
+        reset_fail_counter = py_trees.blackboard.SetBlackboardVariable(
+            name="Set fail counter back to 0",
+            variable_name=f"failcounter {action}",
+            variable_value=0
+        )
 
+        set_action_requested_to_false = py_trees.blackboard.SetBlackboardVariable(
+            name="Set Action requested back to false",
+            variable_name=action,
+            variable_value=False
+        )
 
-    def action_requested(self, action):
-        return self.current_requested_intent == action
+        check_fail_counter = py_trees.blackboard.CheckBlackboardVariable(
+                name=f"{action} failcounter more than 10?",
+                variable_name=f"failcounter {action}",
+                expected_value=10,
+                comparison_operator=operator.ge,
+                debug_feedback_message=True
+            )
+
+        counter_check = py_trees.composites.Sequence(
+            f"Check fail counter",
+            [
+            check_fail_counter,
+            set_action_requested_to_false,
+            reset_fail_counter
+            ]
+        )
+
+        # Increment fail counter
+        incrementor = py_trees.behaviours.Failure(name="IncrementCounter")
+        incrementor.update = (lambda a=action: self.increment_fail_counter(a) or py_trees.common.Status.FAILURE)
+
+        # Add condition and behaviour to tree
+        return py_trees.composites.Selector(
+                f"Execute {action} action if requested", 
+                [
+                    condition,
+                    counter_check,
+                    py_trees.composites.Sequence(
+                        f"Execute {action} action and set action requested to false", 
+                        [
+                        py_trees.composites.Selector(
+                            f"Execute {action} action or increase counter",
+                            [behaviour,
+                                incrementor]
+
+                        ), 
+                        set_action_requested_to_false,
+                        reset_fail_counter
+                        ]),
+                ])  
 
     def shutdown(self):
         rospy.loginfo("Shutting Down Brain Node")
@@ -318,7 +269,7 @@ class Brain:
 if __name__ == '__main__':
     rospy.init_node('brain',anonymous=False)
     node = Brain()
-    rate = rospy.Rate(1)
+    rate = rospy.Rate(5)
 
     while(not rospy.is_shutdown()):
         try:
